b9a634f602770b87514b254d473962188f0acfd6
Harley Hinkle <wolfking068@gmail.com>
2018-05-09 10:09:42 -0700

    sound

---
+++ Assets/loud.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loud : MonoBehaviour
+{
+    public void startgame()
+    {
+        SceneManager.LoadScene(1);  
+    }
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/sound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class sound : MonoBehaviour {
+    public static sound game;
+    public AudioSource vol;
+    public Slider volslid;
+    // Use this for initialization
+    void Start ()
+    {
+        volslid.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+    void Awake()
+    {
+
+        if (game == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            game = this;
+        }
+        else if (game != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+    public void ValueChangeCheck()
+    {
+        PlayerPrefs.SetFloat("VolumeLevel", volslid.value);
+
+        vol.volume = PlayerPrefs.GetFloat("VolumeLevel");
+    }
+}
